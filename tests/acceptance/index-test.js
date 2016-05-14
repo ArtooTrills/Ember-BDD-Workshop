c57e3c8d65700ebe299062ed4f85d21b3e087172
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2016-05-14 13:48:47 +0530

    index page test

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,12 +1,22 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'blr-tdd-workshop/tests/helpers/module-for-acceptance';
+import Pretender from 'pretender';
+import Albums from '../fixtures/albums';
+import Ember from 'ember';
 
 moduleForAcceptance('Acceptance | index');
 
 test('Visiting index page should display albums', function(assert) {
-  visit('/index');
+  visit('/');
+
+  new Pretender(function() {
+    this.get('/albums', function() {
+      return [200, {'content-type': 'application/json'}, JSON.stringify(Albums)];
+    });
+  });
+
 
   andThen(function() {
-    assert.equal(Ember.$('.album').length, 10);
+    assert.equal(Ember.$('.album').length, 12);
   });
 });
